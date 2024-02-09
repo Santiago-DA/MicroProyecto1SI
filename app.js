9eeea17f8f150d2ee3cccf34c070e92316516913
Santiago Aristimuño <sa.aristimuno@correo.unimet.edu.ve>
2024-02-09 16:10:23 -0400

    genereting board

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+
+function genereteBoard(){
+    var size = document.getElementById("bingoSize");
+    console.log(size)
+    //var size =  parseInt(size);
+    //crear el array de la tabla del bingo
+    size = 3
+    for (i=0;i<size*size;i++){
+        var tag = document.createElement("p")
+        var text = document.createTextNode("TEXT");//TODO generete numbers here
+        tag.append(text);
+        var element = document.getElementById('bingoBoard');
+        element.appendChild(tag)
+    }
+}
 function checkValues(){
     var size = document.getElementById("bingoSize").value;
     if ((3<=size) && (5>=size)){
@@ -6,3 +21,8 @@ function checkValues(){
         alert("ingrese un tamaño entre 3 y 5")
     }
 }
+
+
+
+
+
